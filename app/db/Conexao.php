4f9f31a3e9b5323405c9bd06afe7e8d953037cdf
TecOp <rodrigo.froes@futcorp.com.br>
2024-10-09 11:22:58 -0300

    inicio cfg bd

---
+++ app/db/Conexao.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\db;
+
+use PDO;
+
+$host = 'localhost';
+$db = 'clubeLeitura';
+$user = 'root';
+$password = '';
+
+
+class Conexao
+{
+    private $db;
+
+    function __construct()
+    {
+        $dsn = 'mysql:host=' . $host . ';dbname=' . $db  . ';charset=utf8';
+        $this->db = new PDO($dsn, $user, $password);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+    function select($sql, $array = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($array);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function insert($sql, $array = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($array);
+        return $this->db->lastInsertId();
+    }
+
+    function update($sql, $array = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($array);
+        return $stmt->rowCount();
+    }
+
+    function delete($sql, $array = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($array);
+        return $stmt->rowCount();
+    }
+
+    function lastInsertId()
+    {
+        return $this->db->lastInsertId();
+    }
+}
